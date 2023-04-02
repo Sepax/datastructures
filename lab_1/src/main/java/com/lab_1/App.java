a4d020bd4982b3399b436f7131fa1dffda3566dd
Sebastian <seb.plson@gmail.com>
2023-04-02 17:41:45 +0200

    quickstort start

--- lab_1/src/main/java/com/lab_1/App.java
+++ lab_1/src/main/java/com/lab_1/App.java
@@ -1,12 +1,24 @@
 package com.lab_1;
 
+import java.util.Arrays;
+import java.util.Random;
+
 /**
  * Hello world!
  *
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int[] randomArray = { 44, 32, 56, 22, 3, 5, 66, 6, 6, 2, 1, 6, 5, 8, 9, 3, 4, 5, 6, 7, 8, 9, 0, 12, 43 };
+        System.out.println("Original Array:   " + Arrays.toString(randomArray));
+
+        int[] insertArray = Arrays.copyOf(randomArray, randomArray.length);
+        insertionSort(insertArray);
+        System.out.println("Insertion Sorted: " + Arrays.toString(insertArray));
+
+        int[] quickArray = Arrays.copyOf(randomArray, randomArray.length);
+        quickSort(quickArray);
+        System.out.println("Quick Sorted:     " + Arrays.toString(quickArray));
     }
 
     /** Sorting algorithms **/
@@ -26,17 +38,47 @@ public class App {
     // Quicksort.
 
     public static void quickSort(int[] array) {
-        throw new UnsupportedOperationException();
+        quickSort(array, 0, array.length - 1);
     }
 
     // Quicksort part of an array
     private static void quickSort(int[] array, int begin, int end) {
+        if (begin >= end) {
+            return;
+        }
+        int pivot = partition(array, begin, end);
+
+        quickSort(array, begin, pivot - 1);
+        quickSort(array, pivot + 1, end);
     }
 
     // Partition part of an array, and return the index where the pivot
     // ended up.
     private static int partition(int[] array, int begin, int end) {
-        throw new UnsupportedOperationException();
+        Random random = new Random();
+        int pivotIndex = random.nextInt(begin, end);
+        int pivot = array[pivotIndex];
+
+        while (end > begin) {
+
+            while (array[begin] < pivot) {
+                begin++;
+            }
+
+            while (array[end] > pivot) {
+                end--;
+            }
+
+            swap(array, begin, end);
+            begin++;
+            end--;
+        }
+        System.out.println("begin: " + begin);
+        System.out.println("end: " + end);
+        System.out.println(Arrays.toString(array));
+        swap(array, pivotIndex, end);
+
+        return end;
     }
 
     // Swap two elements in an array
