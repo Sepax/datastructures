f7865ab05c4280ff47b362f8cba02c035b0e8120
Sebastian <seb.plson@gmail.com>
2023-05-09 13:25:17 +0200

    main IO start

--- lab3/Main.hs
+++ lab3/Main.hs
@@ -7,6 +7,7 @@
 -}
 
 import AATree
+import System.IO
 
 --------------------------------------------------------------------------------
 
@@ -16,11 +17,24 @@ main = do
 
   -- split the data into words and build an AA tree
   -- use foldl
-  undefined
+  -- Split the text into words
 
+  -- Insert the words into the AA tree
+  let tree = foldl (\acc x -> insert x acc) emptyTree (words contents)
+  
   -- calculate and print statistics
+  let s  = fromIntegral ( size tree )
+  let h  = fromIntegral ( height tree )
+  let h' = fromIntegral ( ceiling ( logBase 2 (s+1) ) - 1 )
+  let r  = h / h' :: Double
+
+  -- Test with checkTree and print result
+  let invariant = undefined
+
   -- use fromIntegral/ceiling/logBase
   undefined
+
+
 
 --------------------------------------------------------------------------------
 
