9d1938615b18144ae52253e9965c5aa6a29ca725
Sebastian <seb.plson@gmail.com>
2023-05-15 16:58:15 +0200

    Graph Module

--- lab_4/Graph.hs
+++ lab_4/Graph.hs
@@ -25,39 +25,40 @@ data Edge a b = Edge
   } deriving Show
 
 -- A graph with nodes of type a and labels of type b.
-data Graph a b = Graph -- TODO: implement a graph with adjacency lists, hint: use a Map.
+data Graph a b = Graph (Map a [Edge a b]) deriving Show
 
--- | Create an empty graph
+-- Create an empty graph
 empty :: Graph a b
-empty = undefined
+empty = Graph (M.empty)
 
--- | Add a vertex (node) to a graph
+-- Add a vertex (node) to a graph
 addVertex :: Ord a => a -> Graph a b -> Graph a b
-addVertex v g = undefined
+addVertex v (Graph map) = Graph (M.insert v [] map)
 
--- | Add a list of vertices to a graph
+-- Add a list of vertices to a graph
 addVertices :: Ord a => [a] -> Graph a b -> Graph a b
-addVertices vs g = undefined
+addVertices [] g = g
+addVertices (v:vs) g = addVertices vs (addVertex v g)
 
--- | Add an edge to a graph, the first parameter is the start vertex (of type a), 
+-- Add an edge to a graph, the first parameter is the start vertex (of type a), 
 -- the second parameter the destination vertex, and the third parameter is the
 -- label (of type b)
 addEdge :: Ord a => a -> a -> b -> Graph a b -> Graph a b
-addEdge v w l = undefined
+addEdge v w l (Graph map) = Graph (M.adjust (\edges -> Edge v w l : edges) v map)
 
--- | Add an edge from start to destination, but also from destination to start,
+-- Add an edge from start to destination, but also from destination to start,
 -- with the same label.
 addBiEdge :: Ord a => a -> a -> b -> Graph a b -> Graph a b
-addBiEdge v w l = undefined
+addBiEdge v w l g = addEdge v w l (addEdge w v l g)
 
--- | Get all adjacent vertices (nodes) for a given node
+-- Get all adjacent vertices (nodes) for a given node
 adj :: Ord a => a -> Graph a b -> [Edge a b]
-adj v g = undefined
+adj v (Graph map) = M.findWithDefault [] v map
 
--- | Get all vertices (nodes) in a graph
+-- Get all vertices (nodes) in a graph
 vertices :: Graph a b -> [a]
-vertices g = undefined
+vertices (Graph map) = M.keys map
 
--- | Get all edges in a graph
+-- Get all edges in a graph
 edges :: Graph a b -> [Edge a b]
-edges g = undefined
+edges (Graph map) = concat (M.elems map)
